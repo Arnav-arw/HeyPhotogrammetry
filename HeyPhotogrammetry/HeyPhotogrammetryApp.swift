4b0ac8c0d986f6035201b0a5d4ce97a98a1b127b
Arnav Singhal <singhal.arnav@gmail.com>
2023-01-26 22:44:23 +0530

    Initial Commit

---
+++ HeyPhotogrammetry/HeyPhotogrammetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeyPhotogrammetryApp.swift
+//  HeyPhotogrammetry
+//
+//  Created by Arnav Singhal on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HeyPhotogrammetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
