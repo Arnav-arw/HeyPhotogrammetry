42ff1c9f19d34dab27cc6a427bbe37fa637ac737
Arnav Singhal <singhal.arnav@gmail.com>
2023-01-26 22:55:29 +0530

    Added app view state model

---
+++ HeyPhotogrammetry/Models/AppViewState.swift
@@ -0,0 +1,15 @@
+//
+//  AppViewState.swift
+//  HeyPhotogrammetry
+//
+//  Created by Arnav Singhal on 26/01/23.
+//
+
+import Foundation
+
+enum AppViewState {
+    case onInputView
+    case onSettingsView
+    case onProcessingView
+    case onExportView
+}
